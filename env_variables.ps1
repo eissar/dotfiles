dfa3cb2f4f0dc72cf55431c9d0cd63c07cd24e89
Eshaan Issar <eshaanissar@outlook.com>
2025-09-28 01:36:11 -0400

    use ~/.config instead of ~/.dotfiles

--- env_variables.ps1
+++ env_variables.ps1
@@ -1,13 +1,13 @@
 
 # Set KOMOREBI_CONFIG_HOME environment variable
-$path = Join-Path $HOME ".dotfiles"
+$path = Join-Path $HOME ".config"
 [System.Environment]::SetEnvironmentVariable(
     "KOMOREBI_CONFIG_HOME",
     $path,
     "User"
 )
 
-$path = Join-Path $HOME ".dotfiles"
+$path = Join-Path $HOME ".config"
 [System.Environment]::SetEnvironmentVariable(
     "WHKD_CONFIG_HOME",
     $path,
